5948931f332555a767e646335f35a084690411a6
Matt Abate <mabate13@gmail.com>
2024-06-27 10:18:32 -0400

    major cleanup of main

--- main.py
+++ main.py
@@ -1,76 +1,32 @@
-import time
-import random
-
-SIZE = 5
 import json
-
 import random
-
+import time
 import tqdm
 
 
-t0 = time.time()
-
-SAVE_FILE = f"results/bests_{int(t0)}.json"
+SIZE = 5
+SAVE_FILE = f"results/bests_{int(time.time())}.json"
 
 with open("states.json") as f:
-    data = json.load(f)
-
-
-states_i_care_about = [
-    "CALIFORNIA",
-    "ARIZONA",
-    "TEXAS",
-    "LOUISIANA",
-    "MISSISSIPPI",
-    "ALABAMA",
-    "FLORIDA",
-]
-
-
-states_i_care_about = [
-    "PENNSYLVANIA",
-]
-
-unique_letters = set()
-for s in states_i_care_about:
-    for c in s:
-        unique_letters.add(c)
-unique_letters = list(unique_letters)
-print(
-    unique_letters
-)  # ['U', 'D', 'L', 'F', 'X', 'B', 'R', 'M', 'O', 'S', 'E', 'I', 'T', 'P', 'A', 'N', 'C', 'Z']
-
-# letters i dont care about
-# [G, H, J, K, Q, V, W, Y]
-
-grid: list[str] = [
-    "VMHWA",
-    "LCISQ",
-    "NOANG",
-    "TIROF",
-    "REGLI",
-]
-# grid = ["".join(random.sample(unique_letters, SIZE)) for _ in range(SIZE)]
-# # mix up letters to make grid
-
-flips = 4
-
-bests = []
-
-annealing_time = 50000
-start_flips = 10
-end_flips = 1
+    JSON_POPULATION: list[list] = json.load(f)
 
 
 def get_kings_moves(pos: tuple[int, int]) -> list[tuple[int, int]]:
+    """Get the possible moves for a king on a chess board.
+
+    Args:
+        pos (tuple[int, int]): the position of the king on the board
+
+    Returns:
+        list[tuple[int, int]]: a list of possible next positions for the king
+    """
     moves = []
     for i in range(-1, 2):
         x = pos[0] + i
         for j in range(-1, 2):
-            y = pos[1] + j
-            if i == 0 and j == 0:
+            if 0 == i == j:  # the king must move
                 continue
+            y = pos[1] + j
             if 0 <= x < SIZE and 0 <= y < SIZE:
                 moves.append((x, y))
 
@@ -116,7 +72,7 @@ def word_possible_at_point(start: tuple[int, int], grid: list[str], word: str):
 def process(grid: list[str]) -> tuple[int, list[str]]:
     score = 0
     states = []
-    for state, population in data:
+    for state, population in JSON_POPULATION:
         possible = False
         for i in range(SIZE):
             for j in range(SIZE):
@@ -124,66 +80,86 @@ def process(grid: list[str]) -> tuple[int, list[str]]:
                     possible = True
                     score += population
                     states.append(state)
-
                     break
             if possible:
                 break
     return score, states
 
 
-cutoff = 165379868
-score, _ = process(grid)
-print("final score:", score, score > cutoff, (score - cutoff) / cutoff * 100, "%")
+def flip_random(grid: list[str], num_flips: int) -> list[str]:
+    """Randomly flip some number of letters in the grid.
 
+    Args:
+        grid (list[str]): the grid to flip
+        num_flips (int): the number of letters to randomize
 
-def random_flip(grid: list[str]) -> list[str]:
+    Returns:
+        list[str]: the new grid"""
     new_grid = grid.copy()
-    x = random.randint(0, SIZE - 1)
-    y = random.randint(0, SIZE - 1)
-    new_grid[x] = new_grid[x][:y] + chr(random.randint(65, 90)) + new_grid[x][y + 1 :]
+    for _ in range(num_flips):
+        x = random.randint(0, SIZE - 1)
+        y = random.randint(0, SIZE - 1)
+        random_letter = chr(random.randint(65, 90))
+        new_grid[x] = new_grid[x][:y] + random_letter + new_grid[x][y + 1 :]
     return new_grid
 
 
-best_score = score
-best_grid = grid.copy()
+if __name__ == "__main__":
+    # states_i_care_about = [
+    #     "CALIFORNIA",
+    #     "ARIZONA",
+    #     "TEXAS",
+    #     "LOUISIANA",
+    #     "MISSISSIPPI",
+    #     "ALABAMA",
+    #     "FLORIDA",
+    # ]
 
+    IMPT_LETTERS = "UDLFXBRMOSTPAZ"
+    # letters i dont care about
+    # [G, H, J, K, Q, V, W, Y]
 
-score, states = process(grid)
-print("------------------")
-print(json.dumps(best_grid, indent=4))
-print(states)
-print("initial score:", "\033[93m", best_score, "\033[0m")
+    # # mix up letters to make grid
+    grid: list[str] = ["".join(random.sample(IMPT_LETTERS, SIZE)) for _ in range(SIZE)]
 
-score, states = process(grid)
+    bests = []
 
+    ANNEALING_TIME = 50000
+    FLIPS_MAX = 10
+    MIN_TEMP = 1
 
-t0 = time.time()
-while True:
-    for tries in tqdm.tqdm(range(annealing_time)):
-        new_grid = best_grid.copy()
-        for i in range(flips):
-            new_grid = random_flip(new_grid)
+    intial_score, _ = process(grid)
+    best_score = intial_score
+    best_grid = grid.copy()
 
-        score, states = process(new_grid)
-        if score > best_score:
-            best_score = score
-            best_grid = new_grid
-            print()
-            print("------------------")
-            print(json.dumps(best_grid, indent=4))
-            print(states)
-            print("new best score:", "\033[93m", best_score, "\033[0m")
+    t0 = time.time()
+    flips = FLIPS_MAX
+    flips_difference = FLIPS_MAX - MIN_TEMP
+    while True:
+        for tries in tqdm.tqdm(range(ANNEALING_TIME)):
+            new_grid = best_grid.copy()
+            new_grid = flip_random(new_grid, flips)
 
-            bests.append((best_score, len(states), best_grid, states))
-            with open(SAVE_FILE, "w") as f:
-                json.dump(bests, f, indent=4)
+            score, states = process(new_grid)
+            if score > best_score:
+                best_score = score
+                best_grid = new_grid
+                print()
+                print("------------------")
+                print(json.dumps(best_grid, indent=4))
+                print(states)
+                print("new best score:", "\033[93m", best_score, "\033[0m")
+
+                bests.append((best_score, len(states), best_grid, states))
+                with open(SAVE_FILE, "w") as f:
+                    json.dump(bests, f, indent=4)
+                break
+
+            # flips == 25 when tries = 0 and flips = 2 when tries = annealing_time
+            flips = int(FLIPS_MAX - (tries / ANNEALING_TIME) * flips_difference)
+            if tries > ANNEALING_TIME:
+                break
+        else:
             break
 
-        # flips == 25 when tries = 0 and flips = 2 when tries = annealing_time
-        flips = int(start_flips - (tries / annealing_time) * (start_flips - end_flips))
-        if tries > annealing_time:
-            break
-    else:
-        break
-
-print("fin", "time:", time.time() - t0, "seconds")
+    print("fin", "time:", time.time() - t0, "seconds")
